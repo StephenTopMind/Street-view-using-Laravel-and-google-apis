a470503e9a86a1ce8e7e57a269ca7be4aeffdc48
Njuguna036 <stvmwg@gmail.com>
2025-04-16 11:58:35 +0300

    Adds .editorconfig file and configures code formatting settings
    Includes .editorconfig file with specified code formatting settings to ensure consistent code style across the project. Sets UTF-8 character encoding, LF line endings, 4-space indentation, and removes trailing whitespace. Overrides the settings for Markdown files and YAML files. Additionally, sets the indentation size to 4 for the docker-compose.yml file.
    
    Fixes #123

---
+++ streetview-app/resources/views/map.blade.php
@@ -0,0 +1,52 @@
+<!-- resources/views/map.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Street View Directions</title>
+<script async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkj4qom_vvXMFCJbKUv3TeaPEC7RqAFPE&libraries=places&loading=async&callback=initMap">
+</script>
+
+    <style>
+        #map, #pano { height: 400px; width: 100%; }
+    </style>
+</head>
+<body>
+    <input id="destination" type="text" placeholder="Enter destination" />
+    <button onclick="calculateRoute()">Get Directions</button>
+    
+    <div id="map"></div>
+    <div id="pano"></div>
+
+    <script>
+    let map, directionsService, directionsRenderer, panorama;
+
+    window.initMap = function () {
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer();
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const origin = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: origin,
+                zoom: 14
+            });
+
+            directionsRenderer.setMap(map);
+
+            panorama = new google.maps.StreetViewPanorama(
+                document.getElementById("pano"),
+                { position: origin, pov: { heading: 165, pitch: 0 }, zoom: 1 }
+            );
+
+            map.setStreetView(panorama);
+        });
+    }
+</script>
+
+</body>
+</html>
